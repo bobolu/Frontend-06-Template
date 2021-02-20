c4b18ebc8ba2d4f975ade09f8c0d40c826ebae69
bobolu <bobolv12@gmail.com>
2021-02-21 04:02:03 +0800

    更新代码作业

--- Week 16/jsx/framework.js
+++ Week 16/jsx/framework.js
@@ -1,4 +1,7 @@
 export function createElement(type, attributes, ...children) {
+
+  // console.log("type:", type);
+
   let element;
   if (typeof type === "string") {
     element = new ElementWrapper(type);
@@ -8,24 +11,33 @@ export function createElement(type, attributes, ...children) {
   for (let name in attributes) {
     element.setAttribute(name, attributes[name]);
   }
-  for (let child of children) {
-    if (typeof child === "string") {
-      child = new TextWrapper(child);
+  let processChildren = (children) => {
+    for (let child of children) {
+      if ((typeof child === "object") && (child instanceof Array)) {
+        processChildren(child);
+        continue;
+      }
+      if (typeof child === "string") {
+        child = new TextWrapper(child);
+      }
+      element.appendChild(child);
     }
-    element.appendChild(child);
-  }
+  };
+  processChildren();
+
   return element;
 }
 
-export const STATE = Symbol('state');
-export const ATTRIBUTE = Symbol('state');
+export const STATE = Symbol("state");
+export const ATTRIBUTE = Symbol("state");
 
 export class Component {
   constructor(type) {
-    // this.root = this.render();
-    // this.attributes = Object.create(null);
     this[STATE] = Object.create(null);
     this[ATTRIBUTE] = Object.create(null);
+  }
+  render() {
+    return this.root;
   }
   setAttribute(name, value) {
     this[ATTRIBUTE][name] = value;
@@ -34,24 +46,31 @@ export class Component {
     child.mountTo(this.root);
   }
   mountTo(parent) {
-    if(!this.root) {
+    if (!this.root) {
       this.render();
     }
     parent.appendChild(this.root);
   }
   triggerEvent(type, args) {
-    this[ATTRIBUTE]["on" + type.replace(/^[\s\S]/, s => s.toUpperCase())](new CustomEvent(type, {detail: args}))
+    this[ATTRIBUTE]["on" + type.replace(/^[\s\S]/, (s) => s.toUpperCase())](
+      new CustomEvent(type, { detail: args })
+    );
   }
 }
 
-class ElementWrapper extends Component{
+class ElementWrapper extends Component {
   constructor(type) {
+    super();
     this.root = document.createElement(type);
   }
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value);
+  }
 }
 
-class TextWrapper extends Component{
+class TextWrapper extends Component {
   constructor(content) {
+    super();
     this.root = document.createTextNode(content);
   }
 }
--- Week 16/jsx/main.js
+++ Week 16/jsx/main.js
@@ -1,22 +1,39 @@
-import { Component, createElement } from "./framework.js";
-import { Carousel } from "./carousel.js";
-import { Timeline, Animation } from "./animation.js";
+// import { Component, createElement } from "./framework.js";
+// import { Carousel } from "./carousel.js";
+// import { Timeline, Animation } from "./animation.js";
 
 let imgs = [
-  {url:"https://www.baidu.com",img:"https://static001.geekbang.org/resource/image/65/9a/6590fb3f37a385b8d88b8679529e9c9a.jpg"},
-  {url:"https://www.taobao.com",img:"https://static001.geekbang.org/static/university/img/banner-bg-content@2x.38f12e0f.png"},
-  {url:"https://www.qq.com",img:"https://static001.geekbang.org/resource/image/7a/30/7a9547384cffa039f063db1fc7669a30.jpg"},
-  {url:"https://www.alipay.com",img:"https://static001.geekbang.org/resource/image/6a/9b/6aec0a09381a2f74014ec604ef99c19b.png"},
-  {url:"https://www.51job.com",img:"https://static001.geekbang.org/resource/image/cb/cb/cbb6d198ccfb95af4906eeb0581333cb.png"}
+  {title:"t1", url:"https://www.baidu.com", img:"https://static001.geekbang.org/resource/image/65/9a/6590fb3f37a385b8d88b8679529e9c9a.jpg"},
+  {title:"t2", url:"https://www.taobao.com", img:"https://static001.geekbang.org/static/university/img/banner-bg-content@2x.38f12e0f.png"},
+  {title:"t3", url:"https://www.qq.com", img:"https://static001.geekbang.org/resource/image/7a/30/7a9547384cffa039f063db1fc7669a30.jpg"},
+  {title:"t4", url:"https://www.alipay.com", img:"https://static001.geekbang.org/resource/image/6a/9b/6aec0a09381a2f74014ec604ef99c19b.png"},
+  {title:"t5", url:"https://www.51job.com", img:"https://static001.geekbang.org/resource/image/cb/cb/cbb6d198ccfb95af4906eeb0581333cb.png"}
 ];
-let a = <Carousel src={imgs} 
-          onChange={event => console.log(event.detail.position) }
-          onClick={event => window.location.href = event.detail.data.url }></Carousel>;
+// let a = <Carousel src={imgs} 
+//           onChange={event => console.log(event.detail.position) }
+//           onClick={event => window.location.href = event.detail.data.url }></Carousel>;
 
-a.mountTo(document.body);
+// a.mountTo(document.body);
 
 // let tl = new Timeline()
 // window.tl = tl;
 // window.animation = new Animation({set a(v) {console.log(v)}},"a",0,100,1000,null);
 // // tl.add(new Animation({set a(v){console.log(v)}},"a",0,100,1000,null));
 // tl.start()
+
+import { Component, createElement } from "./framework.js";
+import { Carousel } from "./carousel.js";
+import { Button } from "./Button.js";
+import { List } from "./List.js";
+// let a = <Button>ss2s</Button>
+// a.mountTo(document.body);
+
+let a = <List data={imgs}>
+{
+  (item) => <div>
+      <img src={item.img}></img>
+      <a href={item.url}>{item.title}</a>
+    </div>
+}
+</List>
+a.mountTo(document.body);
